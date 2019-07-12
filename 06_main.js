8c2921750ea80a4da3f2edcf8eb5cade9956216e
cschier <cschier@uos.de>
2019-07-12 11:59:31 +0200

    add heroku

--- 06_main.js
+++ 06_main.js
@@ -30,13 +30,13 @@ $("document").ready(function() {
         ],
         // Here, you can specify all information for the deployment
         deploy: {
-            experimentID: "INSERT_A_NUMBER",
-            serverAppURL: "https://babe-demo.herokuapp.com/api/submit_experiment/",
+            experimentID: "76",
+            serverAppURL: "https://babe-backend.herokuapp.com/api/submit_experiment/76",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "debug",
-            contact_email: "YOUREMAIL@wherelifeisgreat.you",
+            deployMethod: "directLink",
+            contact_email: "cschier@uos.de",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
         // Here, you can specify how the progress bar should look like
